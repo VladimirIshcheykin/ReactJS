d027a724cba09ea52a184d81235683654ac2ab2a
Vladimir <ishcheykin10@gmail.com>
2022-03-30 20:42:53 +0400

    css-modules - return to css, adjust less to form css files

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,13 +1,13 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
-import styles from './header.less';
+import styles from './header.css';
 
 function HeaderComponent() {
     return (
         <header>
             <h1 className={styles.example}>Hello React!!!</h1>
             <p>This is Table!</p>
-            <table class={styles.table}>
+            <table className={styles.table}>
                 <tbody>
                     <tr>
                         <td>111</td>
